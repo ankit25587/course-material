73a5c45daca904808448cd558e5592aba9015a2d
Ankit Mistry <ankit.25587@gmail.com>
2021-11-12 07:54:57 +0530

    gcf course added

--- 00-gcp-crash-course/code/container/server.js
+++ 00-gcp-crash-course/code/container/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Hello World to Kubernetes v1.0 </h1>");
+  response.end("<h1> Hello World Docker </h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
